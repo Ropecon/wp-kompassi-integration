903c3586d49c5e5fc6ef0bd9a970167c213bcac8
Pasi Lallinaho <pasi@shimmerproject.org>
2023-09-04 19:18:37 +0300

    Add settings GUI (earliest hour for timeline), better time formatting

--- kompassi-integration.php
+++ kompassi-integration.php
@@ -10,11 +10,11 @@
 class WP_Plugin_Kompassi_Integration {
 	function __construct( ) {
 		add_action( 'init', array( &$this, 'init' ) );
+		add_action( 'admin_init', array( &$this, 'admin_init' ) );
+		add_action( 'admin_menu', array( &$this, 'admin_menu' ) );
 		add_action( 'wp_enqueue_scripts', array( &$this, 'wp_enqueue_scripts' ) );
 		add_action( 'enqueue_block_editor_assets', array( &$this, 'enqueue_block_editor_assets' ) );
 		add_filter( 'block_categories_all', array( &$this, 'block_categories_all' ), 10, 2 );
-
-		$this->programmes_to_show = 9999;
 	}
 
 	function init( ) {
@@ -31,9 +31,51 @@ class WP_Plugin_Kompassi_Integration {
 		);
 	}
 
+	function admin_init( ) {
+		add_settings_section( 'kompassi-integration-general', '', '', 'kompassi_integration_settings' );
+
+		$fields = array(
+			'timeline_earliest_hour' => array(
+				'label' =>  __( 'Earliest hour on timeline', 'kompassi-integration' ),
+				'description' => __( 'Earliest hour to show on the timeline, when a single day is selected.', 'kompassi-integration' )
+			)
+		);
+
+		foreach( $fields as $key => $data ) {
+			$data['field_name'] = 'kompassi_integration_' . $key;
+			$data['sanitize_callback'] = 'sanitize_text_field';
+			add_settings_field( 'kompassi_integration_' . $key, $data['label'], array( &$this, 'setting_field' ), 'kompassi_integration_settings', 'kompassi-integration-general', $data );
+			register_setting( 'kompassi_integration_settings', $data['field_name'], $data );
+		}
+	}
+
+	function admin_menu( ) {
+		add_menu_page( __( 'Kompassi Integration', 'kompassi-integration' ), 'Kompassi', 'manage_options', 'kompassi_integration_settings', array( &$this, 'admin_page_settings' ), 'dashicons-book', 100 );
+	}
+
+	function admin_page_settings( ) {
+		echo '<div class="wrap"><form action="options.php" method="POST">';
+		echo '<h1>' . __( 'Kompassi Integration', 'kompassi-integration' ) . '</h1>';
+		settings_fields( 'kompassi_integration_settings' );
+		do_settings_sections( 'kompassi_integration_settings' );
+		submit_button( __( 'Save Changes', 'kompassi-integration' ) );
+		echo '</form></div>';
+	}
+
+	function setting_field( $data ) {
+		echo '<p><input type="text" id="' . $data['field_name'] . '" name="' . $data['field_name'] . '" class="widefat" value="' . get_option( $data['field_name'] ) . '"/></p>';
+		if( isset( $data['description'] ) ) {
+			echo '<p class="description">' . $data['description'] . '</p>';
+		}
+	}
+
 	function wp_enqueue_scripts( ) {
 		wp_enqueue_script( 'kompassi-integration-frontend', plugins_url( 'frontend.js', __FILE__ ), array( 'jquery', 'wp-i18n' ) );
 		wp_set_script_translations( 'kompassi-integration-frontend', 'kompassi-integration', plugin_dir_path( __FILE__ ) . 'languages/' );
+		$js_strings = array(
+			'timeline_earliest_hour' => get_option( 'kompassi_integration_timeline_earliest_hour', 8 )
+		);
+		wp_localize_script( 'kompassi-integration-frontend', 'options', $js_strings );
 		wp_enqueue_style( 'kompassi-integration-frontend', plugins_url( 'frontend.css', __FILE__ ) );
 	}
 
@@ -79,7 +121,7 @@ class WP_Plugin_Kompassi_Integration {
 
 		/*  Programme  */
 		$out .= '<section id="kompassi_programme" class="programme-list timeline">';
-		foreach( array_slice( $prog, 0, $this->programmes_to_show, true ) as $p ) {
+		foreach( $prog as $p ) {
 			$out .= $this->markup_program( $p );
 		}
 		$out .= '</section>';
@@ -134,11 +176,12 @@ class WP_Plugin_Kompassi_Integration {
 					$show_keys = array(
 							'title' => '<strong>%s</strong>',
 							'room_name' => '%s',
-							'start_time' => '%s',
-							'end_time' => '%s',
+//							'start_time' => '%s',
+//							'end_time' => '%s',
+							'times' => '',
 							'description' => '%s',
 							'category_title' => '%s',
-							'formatted_hosts' => 'Hosts: %s',
+							'formatted_hosts' => '%s',
 					);
 					foreach( $show_keys as $key => $format ) {
 						if( isset( $programme[$key] ) ) {
@@ -146,6 +189,23 @@ class WP_Plugin_Kompassi_Integration {
 							if( is_array( $value ) ) {
 								$value = implode( ',', $value );
 							}
+						} else {
+							$format = '%s';
+							// TODO: Get directly from Kompassi?
+							if( 'times' == $key ) {
+								$start_date = date_i18n( 'j.n.Y', strtotime( $programme['start_time'] ) );
+								$end_date = date_i18n( 'j.n.Y', strtotime( $programme['end_time'] ) );
+
+								$value = date_i18n( 'D j.n. H.i', strtotime( $programme['start_time'] ) );
+								$value .= ' – ';
+								if( $start_date == $end_date ) {
+									$value .= date_i18n( 'H.i', strtotime( $programme['end_time'] ) );
+								} else {
+									$value .= date_i18n( 'D j.n. H.i', strtotime( $programme['end_time'] ) );
+								}
+							}
+						}
+						if( isset( $value ) ) {
 							echo '<div class="entry ' . $key . '" style="grid-area: ' . $key . ';">' . sprintf( $format, $value ) . '</div>';
 						}
 					}
