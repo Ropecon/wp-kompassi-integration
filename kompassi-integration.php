29fec8c7bf7e5c199d717ae79c2aac52b61ec5ca
Pasi Lallinaho <pasi@shimmerproject.org>
2025-07-25 09:59:14 +0300

    Fix: Return programs to chronological order when moving from timetable to list on mobile

--- kompassi-integration.php
+++ kompassi-integration.php
@@ -3,7 +3,7 @@
  *  Plugin Name: Kompassi Integration
  *  Description: Integrate data from Kompassi to your WordPress site easily
  *  Author: Pasi Lallinaho
- *  Version: 2025-07-11
+ *  Version: 2025-07-25
  *  Text Domain: kompassi-integration
  *
  */
